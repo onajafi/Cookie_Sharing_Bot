ca32331545c5452930f2bcaaf52201ea0141a869
omid <onajafi141@gmail.com>
2017-07-12 20:22:44 +0430

    tried some inline query stuff

--- InlineBtn.py
+++ InlineBtn.py
@@ -26,10 +26,10 @@ def callbacks(call):
     print call
     print "and by the user: " + call.from_user.username + ' ' + call.from_user.first_name + ' ' + call.from_user.last_name
 
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("OMGGG!!!", callback_data="CALLB_DA"))
-    markup.add(types.InlineKeyboardButton("Yahoo", url="http://www.yahoo.com"))
-    bot.edit_message_text("EDITED!!!",chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+    # markup = types.InlineKeyboardMarkup()
+    # markup.add(types.InlineKeyboardButton("OMGGG!!!", callback_data="Done"))
+    # markup.add(types.InlineKeyboardButton("Yahoo", url="http://www.yahoo.com"))
+    # bot.edit_message_text("EDITED!!!",chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
 
 
 @bot.message_handler(commands=['start'])
@@ -37,10 +37,25 @@ def send_welcome(message):
     #reply = "Yello!!!"
     #bot.reply_to(message, reply)
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Google",callback_data="CALLB_DA"))
+    markup.add(types.InlineKeyboardButton("Google",callback_data="CALLB_DA", switch_inline_query="Hi"))
     markup.add(types.InlineKeyboardButton("Yahoo", url="http://www.yahoo.com"))
     bot.send_message(message.chat.id, "SAMPLE TEXT", disable_notification=True, reply_markup=markup)
+    print message.text
 
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    print "got " + inline_query.data
+
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    print "hehehe"
+    try:
+        print "imhere"
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
 
 
 # @bot.inline_handler(lambda query: len(query.query) is 0)
